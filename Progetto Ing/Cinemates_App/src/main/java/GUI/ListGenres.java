b13e50f745a68d304395613ca970f7a640245c94
Vincenzo Di Meo <74734387+vincdimeo@users.noreply.github.com>
2021-02-12 18:42:49 +0100

    Add files via upload

---
+++ Progetto Ing/Cinemates_App/src/main/java/GUI/ListGenres.java
@@ -0,0 +1,13 @@
+package GUI;
+
+public class ListGenres {
+    private String genre;
+
+    public ListGenres(String genre) {
+        this.genre = genre;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+}
