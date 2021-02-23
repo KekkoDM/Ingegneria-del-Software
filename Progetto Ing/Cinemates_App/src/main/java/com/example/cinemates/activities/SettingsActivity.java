6a4a3e5ce99b070d8cb6e1acc6836b9189e36603
Vincenzo Di Meo <74734387+vincdimeo@users.noreply.github.com>
2021-02-23 17:53:51 +0100

    Add files via upload

---
+++ Progetto Ing/Cinemates_App/src/main/java/com/example/cinemates/activities/SettingsActivity.java
@@ -0,0 +1,32 @@
+package com.example.cinemates.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.example.cinemates.R;
+
+public class SettingsActivity extends AppCompatActivity {
+    private EditText oldPw;
+    private EditText newPw;
+    private ImageButton backBtn;
+    private Button updateBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        backBtn = findViewById(R.id.backBtnSettings);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
---
+++ Progetto Ing/Cinemates_App/src/main/java/com/example/cinemates/fragments/SearchFragment.java
@@ -0,0 +1,120 @@
+package com.example.cinemates.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.cinemates.R;
+import com.example.cinemates.activities.ResultsActivity;
+import com.example.cinemates.adapters.SearchSuggestionsAdapter;
+import com.example.cinemates.classes.SearchSuggestion;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class SearchFragment extends Fragment {
+    private EditText query;
+    private Button searchButton;
+    private RecyclerView rv;
+    private SearchSuggestionsAdapter adapter;
+    private ArrayList<SearchSuggestion> list;
+    private RequestQueue requestQueue;
+
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        rv = view.findViewById(R.id.recyclerView);
+        rv.setHasFixedSize(true);
+        rv.setLayoutManager(new GridLayoutManager(this.getContext(), 2));
+
+        list = new ArrayList<>();
+
+        requestQueue = Volley.newRequestQueue(this.getContext());
+        parseJSON();
+
+        query = view.findViewById(R.id.textSearched);
+        searchButton = view.findViewById(R.id.searchBtn);
+
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (query.getText().length() == 0) {
+                    query.setHintTextColor(getResources().getColor(R.color.google_color));
+                }
+                else {
+                    query.setHintTextColor(getResources().getColor(R.color.light_grey));
+                    Intent intent = new Intent(getActivity(), ResultsActivity.class);
+                    intent.putExtra("textSearched", query.getText().toString());
+                    startActivity(intent);
+                }
+            }
+        });
+
+        return view;
+    }
+
+    private void parseJSON() {
+        String url = "https://api.themoviedb.org/3/trending/all/day?api_key=d6f6fde62b39251f66a180f2c13ac19f";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("results");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject hit = jsonArray.getJSONObject(i);
+
+                                String title = "";
+                                String type = hit.getString("media_type");
+
+                                switch (type) {
+                                    case "movie":
+                                        title = hit.getString("title");
+                                        break;
+
+                                    case "tv":
+                                        title = hit.getString("name");
+                                        break;
+                                }
+
+                                String cover = hit.getString("backdrop_path");
+                                list.add(new SearchSuggestion(cover, title));
+                            }
+                            adapter = new SearchSuggestionsAdapter(SearchFragment.this.getContext(), list);
+                            rv.setAdapter(adapter);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+        requestQueue.add(request);
+    }
+}
