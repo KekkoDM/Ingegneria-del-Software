bc61dae30e17f8bf64dbb65c43561f8dc9c31968
AntonioNavarra <75014356+AntonioNavarra@users.noreply.github.com>
2021-03-03 19:28:25 +0100

    Add files via upload

---
+++ Cinemates_Navarr/DescriptionFragment.java
@@ -0,0 +1,27 @@
+package com.example.cinemates.fragments;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.cinemates.R;
+
+public class DescriptionFragment extends Fragment {
+
+    public DescriptionFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
+    }
+}
