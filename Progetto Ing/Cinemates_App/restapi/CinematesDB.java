c48651bef20be047d14dd9da2b8e8312b3561f35
Vincenzo Di Meo <74734387+vincdimeo@users.noreply.github.com>
2021-03-03 20:54:41 +0100

    Add files via upload

--- Progetto Ing/Cinemates_App/restapi/CinematesDB.java
+++ Progetto Ing/Cinemates_App/restapi/CinematesDB.java
@@ -2,9 +2,10 @@ package com.example.cinemates.restapi;
 
 public class CinematesDB {
     private static final String BASE_URL = "https://cinematesphp.000webhostapp.com/cinematesREST/";
-    public static final String REGISTER_URL = BASE_URL + "utenteCRUD/create.php";
-    public static final String LOGIN_URL = BASE_URL + "utenteCRUD/read.php";
-    public static final String UPDATE_URL = BASE_URL + "utenteCRUD/update.php";
-    public static final String DELETE_URL = BASE_URL + "utenteCRUD/delete.php";
-    public static final String FRIENDS_URL = BASE_URL + "utenteCRUD/myfriends.php";
+    public static final String REGISTER_URL = BASE_URL + "utenteCRUD/register.php";
+    public static final String LOGIN_URL = BASE_URL + "utente/login.php";
+    public static final String UPDATE_URL = BASE_URL + "utente/updatePassword.php";
+    public static final String DELETE_URL = BASE_URL + "utente/delete.php";
+    public static final String FRIENDS_URL = BASE_URL + "utente/getFriendsList.php";
+    public static final String NOTIFICATION_URL = BASE_URL + "notifica/getNotifications.php";
 }
