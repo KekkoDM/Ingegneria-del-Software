98187f8fc2ea5e20911ac1f943d912cacd8ae7ec
franc.demarco <59506453+KekkoDM@users.noreply.github.com>
2021-03-12 18:43:49 +0100

    Add files via upload

---
+++ Progetto Ing/Cinemates_App/restapi/CinematesDB.java
@@ -0,0 +1,11 @@
+package com.example.cinemates.restapi;
+
+public class CinematesDB {
+    private static final String BASE_URL = "https://cinematesphp.000webhostapp.com/cinematesREST/";
+    public static final String REGISTER_URL = BASE_URL + "utente/register.php";
+    public static final String LOGIN_URL = BASE_URL + "utente/login.php";
+    public static final String UPDATE_URL = BASE_URL + "utente/updatePassword.php";
+    public static final String DELETE_URL = BASE_URL + "utente/delete.php";
+    public static final String FRIENDS_URL = BASE_URL + "utente/getFriendsList.php";
+    public static final String NOTIFICATION_URL = BASE_URL + "notifica/getNotifications.php";
+}
