ec6dd6f057e8329b3d5f7f68efd8beb5282c77a9
franc.demarco <59506453+KekkoDM@users.noreply.github.com>
2021-03-20 11:42:11 +0100

    Update CommentsActivity.java

--- Progetto Ing/Cinemates_App/activities/CommentsActivity.java
+++ Progetto Ing/Cinemates_App/activities/CommentsActivity.java
@@ -1,18 +1,22 @@
-package com.example.cinemates_app.activities;
+package com.example.cinemates.activities;
 import com.example.cinemates.adapters.ReviewAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.example.cinemates.classes.Film;
 import com.example.cinemates.classes.Review;
 import com.example.cinemates.R;
 
 public class CommentsActivity extends AppCompatActivity {
     private RecyclerView rvComments;
     private TextView detailReview;
-    private TextView usarnameReview;
+    private TextView usernameReview;
     private TextView dateReview;
     private TextView contLike;
     private TextView contDislike;
@@ -32,9 +36,18 @@ public class CommentsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comments);
 
+        Intent intent = getIntent();
+        review = (Review) intent.getSerializableExtra("recensione");
+
         detailReview = findViewById(R.id.detail_review_comment);
-        usarnameReview = findViewById(R.id.username_review_comment);
+        detailReview.setText(review.getDescrizione());
+
+        usernameReview = findViewById(R.id.username_review_comment);
+        usernameReview.setText(review.getUser());
+
         dateReview = findViewById(R.id.date_review_comment);
+        dateReview.setText(review.getData());
+
         contLike = findViewById(R.id.cont_like_comment);
         contDislike = findViewById(R.id.cont_dislike_comment);
         like = findViewById(R.id.like_comment);
