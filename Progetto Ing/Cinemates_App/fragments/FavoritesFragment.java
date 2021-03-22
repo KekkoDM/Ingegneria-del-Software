444ae04d3d41d133fc24904be92f99a366436f79
AntonioNavarra <75014356+AntonioNavarra@users.noreply.github.com>
2021-03-22 18:54:02 +0100

    Add files via upload

--- Progetto Ing/Cinemates_App/fragments/FavoritesFragment.java
+++ Progetto Ing/Cinemates_App/fragments/FavoritesFragment.java
@@ -1,16 +1,23 @@
 package com.example.cinemates.fragments;
 
 import android.os.Bundle;
-
+import com.example.cinemates.adapters.FilmAdapter;
 import androidx.fragment.app.Fragment;
-
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.example.cinemates.R;
+import com.example.cinemates.classes.Film;
+
+import java.util.ArrayList;
 
 public class FavoritesFragment extends Fragment {
+    private RecyclerView recyclerViewToSee;
+    private RecyclerView recyclerViewFavorites;
+    private FilmAdapter filmAdapter;
+    public static ArrayList<Film> listFilm;
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -20,6 +27,22 @@ public class FavoritesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false);
+        View view = inflater.inflate(R.layout.fragment_favorites, container, false);
+
+        recyclerViewToSee = view.findViewById(R.id.list_film_tosee);
+        recyclerViewToSee.setHasFixedSize(true);
+        recyclerViewFavorites = view.findViewById(R.id.list_film_favorites);
+        recyclerViewFavorites.setHasFixedSize(true);
+
+        LinearLayoutManager ll = new LinearLayoutManager(this.getContext());
+        ll.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        LinearLayoutManager llm = new LinearLayoutManager(this.getContext());
+        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
+
+        recyclerViewToSee.setLayoutManager(ll);
+        recyclerViewFavorites.setLayoutManager(llm);
+
+        return view;
     }
 }
