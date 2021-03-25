cdf0bf44a698aa7543c78815cbfe75a30fee1de8
Vincenzo Di Meo <vinc.dimeo@studenti.unina.it>
2021-03-25 17:26:26 +0100

    Update FavoritesFragment.java

--- Progetto Ing/Cinemates_App/fragments/FavoritesFragment.java
+++ Progetto Ing/Cinemates_App/fragments/FavoritesFragment.java
@@ -1,6 +1,13 @@
 package com.example.cinemates.fragments;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+
+import com.android.volley.RequestQueue;
+import com.example.cinemates.MainActivity;
 import com.example.cinemates.adapters.FilmAdapter;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -8,16 +15,28 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
 import com.example.cinemates.R;
+import com.example.cinemates.adapters.GeneralNotificationAdapter;
+import com.example.cinemates.api.CinematesDB;
 import com.example.cinemates.classes.Film;
+import com.example.cinemates.classes.Notifica;
+import com.example.cinemates.classes.RequestJson;
+import com.example.cinemates.handlers.RequestHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FavoritesFragment extends Fragment {
     private RecyclerView recyclerViewToSee;
     private RecyclerView recyclerViewFavorites;
     private FilmAdapter filmAdapter;
-    public static ArrayList<Film> listFilm;
+    public static ArrayList<Film> listItem;
 
     public FavoritesFragment() {
         // Required empty public constructor
@@ -31,6 +50,7 @@ public class FavoritesFragment extends Fragment {
 
         recyclerViewToSee = view.findViewById(R.id.list_film_tosee);
         recyclerViewToSee.setHasFixedSize(true);
+
         recyclerViewFavorites = view.findViewById(R.id.list_film_favorites);
         recyclerViewFavorites.setHasFixedSize(true);
 
@@ -43,6 +63,73 @@ public class FavoritesFragment extends Fragment {
         recyclerViewToSee.setLayoutManager(ll);
         recyclerViewFavorites.setLayoutManager(llm);
 
+        listItem = new ArrayList<>();
+
+        loadList(recyclerViewFavorites, CinematesDB.LIST_FAVORITES_URL);
+        loadList(recyclerViewToSee, CinematesDB.LIST_TO_SEE_URL);
+
         return view;
+    }
+
+    private void loadList(RecyclerView recyclerView, String url) {
+        class ListLoader extends AsyncTask<Void, Void, String> {
+
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                //creating request handler object
+                RequestHandler requestHandler = new RequestHandler();
+
+                //creating request parameters
+                HashMap<String, String> params = new HashMap<>();
+                params.put("username", MainActivity.utente.getUsername());
+
+                //returing the response
+                return requestHandler.sendPostRequest(url, params);
+            }
+
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+
+                try {
+                    //converting response to json object
+                    JSONObject obj = new JSONObject(s);
+
+                    //if no error in response
+                    if (!obj.getBoolean("error")) {
+                        RequestJson requestJson = new RequestJson(getContext());
+                        JSONArray list = obj.getJSONArray("list");
+                        for (int i = 0; i < list.length(); i++) {
+                            JSONObject film = list.getJSONObject(i);
+                            Film item = new Film(
+                                    film.getString("id"),
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    null,
+                                    film.getString("type")
+                            );
+                            listItem.add(item);
+                            System.out.println("FILM: " +item.getId() +" "+item.getType());
+                            requestJson.parseJSONSavedList(recyclerView, filmAdapter, item);
+                        }
+                    } else {
+                        Toast.makeText(getContext(), obj.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        ListLoader listLoader = new ListLoader();
+        listLoader.execute();
     }
 }
