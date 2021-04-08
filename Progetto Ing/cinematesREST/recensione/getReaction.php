fde1147126c99baa5bdb4fbbe7a32f45fd1a3ac1
Vincenzo Di Meo <74734387+vincdimeo@users.noreply.github.com>
2021-04-08 17:32:01 +0000

    Add files via upload

--- Progetto Ing/cinematesREST/recensione/getReaction.php
+++ Progetto Ing/cinematesREST/recensione/getReaction.php
@@ -14,23 +14,24 @@ $stmt->store_result();
 if($stmt->num_rows > 0) {
   $stmt->bind_result($reaction);
   $stmt->fetch();
-
-  $stmt = $conn->prepare("SELECT COUNT(valutazione) FROM valutazione WHERE recensione = ?");
-  $stmt->bind_param("s", $review);
-  $stmt->execute();
-  $stmt->store_result();
-  $stmt->bind_result($cont);
-  $stmt->fetch();
-
-  $response['error'] = false;
-  $response['message'] = 'Reazione caricata correttamente';
   $response['reazione'] = $reaction;
-  $response['contatore'] = $cont;
 }
 else {
   $response['error'] = true;
   $response['message'] = 'Non hai reagito a questa recensione';
 }
+
+$stmt = $conn->prepare("SELECT COUNT(valutazione) FROM valutazione WHERE recensione = ?");
+$stmt->bind_param("s", $review);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($cont);
+$stmt->fetch();
+
+$response['error'] = false;
+$response['message'] = 'Reazione caricata correttamente';
+$response['contatore'] = $cont;
+
 $stmt->close();
 echo json_encode($response);
 ?>
