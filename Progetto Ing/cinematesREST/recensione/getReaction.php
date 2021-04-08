c15a7b3dabeee1391aa0ba1b26c4a82a88f0d878
Vincenzo Di Meo <74734387+vincdimeo@users.noreply.github.com>
2021-04-08 17:54:32 +0000

    Add files via upload

--- Progetto Ing/cinematesREST/recensione/getReaction.php
+++ Progetto Ing/cinematesREST/recensione/getReaction.php
@@ -5,22 +5,6 @@ $response = array();
 $username = $_POST['username'];
 $review = $_POST['review'];
 
-// checking if the user has already reacted
-$stmt = $conn->prepare("SELECT valutazione FROM valutazione WHERE utente = ? AND recensione = ?");
-$stmt->bind_param("ss", $username, $review);
-$stmt->execute();
-$stmt->store_result();
-
-if($stmt->num_rows > 0) {
-  $stmt->bind_result($reaction);
-  $stmt->fetch();
-  $response['reazione'] = $reaction;
-}
-else {
-  $response['error'] = true;
-  $response['message'] = 'Non hai reagito a questa recensione';
-}
-
 $stmt = $conn->prepare("SELECT COUNT(valutazione) FROM valutazione WHERE recensione = ?");
 $stmt->bind_param("s", $review);
 $stmt->execute();
@@ -28,10 +12,26 @@ $stmt->store_result();
 $stmt->bind_result($cont);
 $stmt->fetch();
 
-$response['error'] = false;
-$response['message'] = 'Reazione caricata correttamente';
-$response['contatore'] = $cont;
+// checking if the user has already reacted
+$stmt = $conn->prepare("SELECT valutazione FROM valutazione WHERE utente = ? AND recensione = ?");
+$stmt->bind_param("ss", $username, $review);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($reaction);
+$stmt->fetch();
 
+if($stmt->num_rows > 0) {
+  $response['error'] = false;
+  $response['message'] = 'Reazione caricata correttamente';
+  $response['reazione'] = $reaction;
+  $response['contatore'] = $cont;
+}
+else {
+  $response['error'] = true;
+  $response['message'] = 'Non hai reagito a questa recensione';
+  $response['reazione'] = $reaction;
+  $response['contatore'] = $cont;
+}
 $stmt->close();
 echo json_encode($response);
 ?>
