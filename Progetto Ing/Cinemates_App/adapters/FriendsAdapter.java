31a32d4fada4d862830a88853d95bcbc6cf52a42
Vincenzo Di Meo <vinc.dimeo@studenti.unina.it>
2021-04-12 17:39:51 +0200

    Update FriendsAdapter.java

--- Progetto Ing/Cinemates_App/adapters/FriendsAdapter.java
+++ Progetto Ing/Cinemates_App/adapters/FriendsAdapter.java
@@ -1,14 +1,19 @@
 package com.example.cinemates.adapters;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
+import androidx.fragment.app.FragmentManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.cinemates.MainActivity;
@@ -17,6 +22,8 @@ import com.example.cinemates.activities.FriendsActivity;
 import com.example.cinemates.activities.ResultsActivity;
 import com.example.cinemates.api.CinematesDB;
 import com.example.cinemates.classes.Utente;
+import com.example.cinemates.fragments.AccountFragment;
+import com.example.cinemates.fragments.FollowNotificationsFragment;
 import com.example.cinemates.handlers.RequestHandler;
 
 import org.json.JSONArray;
@@ -44,14 +51,23 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.MyViewHo
     @Override
     public void onBindViewHolder(@NonNull FriendsAdapter.MyViewHolder holder, int position) {
         Utente friend = friends.get(position);
+
         holder.setFriend(friend);
-        holder.mSharedContent.setOnClickListener(new View.OnClickListener() {
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(context, ResultsActivity.class);
                 intent.putExtra("type", "sharedcontent");
                 intent.putExtra("Friend", friends.get(position).getUsername());
                 context.startActivity(intent);
+            }
+        });
+
+        holder.mRemoveFriend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                holder.removeFriend(friend, position);
             }
         });
     }
@@ -64,18 +80,66 @@ public class FriendsAdapter extends RecyclerView.Adapter<FriendsAdapter.MyViewHo
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView mFriendUsername;
         public TextView mFriendName;
-        public Button mSharedContent;
+        public Button mRemoveFriend;
 
         public MyViewHolder(View itemView) {
             super(itemView);
             mFriendUsername = itemView.findViewById(R.id.usernameFriend);
             mFriendName = itemView.findViewById(R.id.friendNameSurname);
-            mSharedContent = itemView.findViewById(R.id.showSharedBtn);
+            mRemoveFriend = itemView.findViewById(R.id.removeFriendBtn);
         }
 
         public void setFriend(Utente friend) {
             mFriendUsername.setText(friend.getUsername());
             mFriendName.setText(friend.getNome() + " " + friend.getCognome());
+        }
+
+        private void removeFriend(Utente friend, int position) {
+            class FriendRemover extends AsyncTask<Void, Void, String> {
+                @Override
+                protected void onPreExecute() {
+                    super.onPreExecute();
+                }
+
+                @Override
+                protected String doInBackground(Void... voids) {
+                    //creating request handler object
+                    RequestHandler requestHandler = new RequestHandler();
+
+                    //creating request parameters
+                    HashMap<String, String> params = new HashMap<>();
+                    params.put("username", MainActivity.utente.getUsername());
+                    params.put("friend", friend.getUsername());
+
+                    //returning the response
+                    return requestHandler.sendPostRequest(CinematesDB.REMOVE_FRIEND, params);
+                }
+
+                @Override
+                protected void onPostExecute(String s) {
+                    super.onPostExecute(s);
+
+                    try {
+                        //converting response to json object
+                        JSONObject obj = new JSONObject(s);
+
+                        //if no error in response
+                        if (!obj.getBoolean("error")) {
+                            friends.remove(position);
+                            FriendsActivity.rv.removeViewAt(position);
+                            FriendsActivity.friendsAdapter.notifyItemRemoved(position);
+                            FriendsActivity.friendsAdapter.notifyItemRangeChanged(position, friends.size());
+                        }
+
+                        Toast.makeText(context, obj.getString("message"), Toast.LENGTH_SHORT).show();
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            FriendRemover friendRemover = new FriendRemover();
+            friendRemover.execute();
         }
     }
 }
