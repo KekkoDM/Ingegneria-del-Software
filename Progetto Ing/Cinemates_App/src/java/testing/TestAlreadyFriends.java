face0dd752c771cf2314983f48adb4e6b55c1b3c
franc.demarco <59506453+KekkoDM@users.noreply.github.com>
2021-04-27 15:51:01 +0200

    Update TestAlreadyFriends.java

--- Progetto Ing/Cinemates_App/src/java/testing/TestAlreadyFriends.java
+++ Progetto Ing/Cinemates_App/src/java/testing/TestAlreadyFriends.java
@@ -27,7 +27,7 @@ public class TestAlreadyFriends {
     @Test
     public void alreadyFriendsTrue(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
@@ -44,7 +44,7 @@ public class TestAlreadyFriends {
     @Test
     public void alreadyFriendsFalse(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
@@ -54,14 +54,14 @@ public class TestAlreadyFriends {
 
         SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
 
-        Utente user = new Utente("balto",null,null,null,null);
+        Utente user = new Utente("balto");
         Assert.assertFalse(sa.alreadyFriends(user));
     }
 
     @Test(expected = NullPointerException.class)
     public void alreadyFriendsListFriendsNull(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
@@ -71,14 +71,14 @@ public class TestAlreadyFriends {
 
         SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
 
-        Utente user = new Utente("balto",null,null,null,null);
+        Utente user = new Utente("balto");
         Assert.assertTrue(sa.alreadyFriends(user));
     }
 
     @Test(expected = Error.class)
     public void alreadyFriendsListSearchNull(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
@@ -94,7 +94,7 @@ public class TestAlreadyFriends {
     @Test(expected = NullPointerException.class)
     public void alreadyFriendsUserNull(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
@@ -110,16 +110,159 @@ public class TestAlreadyFriends {
     @Test
     public void alreadyFriendsNoFriend(){
 
-        Utente friend = new Utente("vincy99",null,null,null,null);
+        Utente friend = new Utente("vincy99");
         MainActivity ma = new MainActivity();
         MainActivity.selectedFragment = new HomeFragment();
         ArrayList<Utente> friends = new ArrayList<>();
 
-
         FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
-
         SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
 
         Assert.assertFalse(sa.alreadyFriends(friend));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void alreadyFriendsFriendListNullSearchNull(){
+
+        Utente friend = new Utente("vincy99");
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),null);
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),null);
+
+        Assert.assertFalse(sa.alreadyFriends(friend));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void alreadyFriendsFriendListNullSearchNullFriendNull(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),null);
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),null);
+
+        Assert.assertFalse(sa.alreadyFriends(null));
+    }
+
+    @Test
+    public void alreadyFriendsFalseMultiFriends(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertFalse(sa.alreadyFriends(new Utente("PerCoca")));
+    }
+
+    @Test
+    public void alreadyFriendsTrueMultiFriends(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+        friends.add(new Utente("Carlo"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertTrue(sa.alreadyFriends(new Utente("balto")));
+    }
+
+    @Test
+    public void alreadyFriendsMultiFriendsNoResult(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+        friends.add(new Utente("Carlo"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        friends.clear();
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertFalse(sa.alreadyFriends(new Utente("balto")));
+    }
+
+
+    @Test
+    public void alreadyFriendsTrueMultiFriendsMultiResult(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+        friends.add(new Utente("Carlo"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        friends.clear();
+        friends.add(new Utente("io"));
+        friends.add(new Utente("tu"));
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("noi"));
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertTrue(sa.alreadyFriends(new Utente("vincy99")));
+    }
+
+    @Test
+    public void alreadyFriendsFalseMultiFriendsMultiResult(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+        friends.add(new Utente("Carlo"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        friends.clear();
+        friends.add(new Utente("io"));
+        friends.add(new Utente("tu"));
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("noi"));
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertFalse(sa.alreadyFriends(new Utente("balto")));
+    }
+
+    @Test
+    public void alreadyFriendsFalseMultiFriendsMultiResultUserVuoto(){
+
+        MainActivity ma = new MainActivity();
+        MainActivity.selectedFragment = new HomeFragment();
+        ArrayList<Utente> friends = new ArrayList<>();
+
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("balto"));
+        friends.add(new Utente("Carlo"));
+
+        FriendsAdapter friendsAdapter = new FriendsAdapter(MainActivity.selectedFragment.getContext(),friends);
+        friends.clear();
+        friends.add(new Utente("io"));
+        friends.add(new Utente("tu"));
+        friends.add(new Utente("vincy99"));
+        friends.add(new Utente("noi"));
+        SearchUserAdapter sa = new SearchUserAdapter( MainActivity.selectedFragment.getContext(),friends);
+
+        Assert.assertFalse(sa.alreadyFriends(new Utente("")));
     }
 }
